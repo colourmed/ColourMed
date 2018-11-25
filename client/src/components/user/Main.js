191f123f91c6e82b1587d37bb13f157a4f587c7d
Cristi <cristicismas99@gmail.com>
2018-11-25 14:20:01 +0200

    Add featured products slider to the user interface

--- client/src/components/user/Main.js
+++ client/src/components/user/Main.js
@@ -2,27 +2,34 @@ import React, { Component } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchRobes } from '../../store/actions/robes';
+import { fetchFeatured } from '../../store/actions/featured';
 import { removeError } from '../../store/actions/errors';
 import { removeSuccess } from '../../store/actions/success';
 import '../../css/universal/Main.css';
 
 import Error from '../universal/Error';
 import Success from '../universal/Success';
-import Robes from '../universal/Robes';
+import Products from './Products';
 import Cart from './Cart';
 import Login from './Login';
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      robes: []
-    };
+  componentWillMount() {
+    this.props.fetchRobes();
   }
 
   render() {
-    const { errors, success, history, robes, removeSuccess, removeError } = this.props;
+    const {
+      errors,
+      success,
+      history,
+      robes,
+      fetchRobes,
+      featured,
+      fetchFeatured,
+      removeSuccess,
+      removeError
+    } = this.props;
 
     const Root = () => <h1>ROOT</h1>;
     const Contact = () => <h1>Contact</h1>;
@@ -34,15 +41,35 @@ class Main extends Component {
 
         <Switch>
           <Route exact path="/" component={Root} />
+
           <Route exact path="/contact" component={Contact} />
-          <Route exact path="/cart" render={() => <Cart history={history} removeError={removeError} removeSuccess={removeSuccess} />} />
+
+          <Route
+            exact
+            path="/cart"
+            render={() => (
+              <Cart
+                history={history}
+                removeError={removeError}
+                removeSuccess={removeSuccess}
+              />
+            )}
+          />
+
           <Route
             exact
             path="/products"
             render={() => (
-              <Robes robes={robes} history={history} showUserControls={true} />
+              <Products
+                history={history}
+                robes={robes}
+                fetchRobes={fetchRobes}
+                featured={featured}
+                fetchFeatured={fetchFeatured}
+              />
             )}
           />
+
           <Route exact path="/login" component={Login} />
         </Switch>
       </div>
@@ -52,6 +79,8 @@ class Main extends Component {
 
 function mapStateToProps(state) {
   return {
+    featured: state.featured,
+    robes: state.robes,
     errors: state.errors,
     success: state.success
   };
@@ -60,6 +89,6 @@ function mapStateToProps(state) {
 export default withRouter(
   connect(
     mapStateToProps,
-    { fetchRobes, removeSuccess, removeError }
+    { fetchRobes, fetchFeatured, removeSuccess, removeError }
   )(Main)
 );
---
+++ client/src/components/user/Products.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import Slider from 'react-slick';
+import '../../css/user/Products.css';
+
+import FullRobe from '../universal/FullRobe';
+import Robes from '../universal/Robes';
+
+class Products extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      featuredItems: []
+    };
+  }
+
+  componentWillMount() {
+    this.props.fetchFeatured();
+  }
+
+  componentDidUpdate(prevProps) {
+    const { robes, featured } = this.props;
+    let featuredItems = [];
+
+    if (prevProps.featured !== featured || prevProps.robes !== robes) {
+      if (robes) {
+        for (let robe of robes) {
+          if (featured.includes(robe._id)) {
+            featuredItems.push(robe);
+          }
+        }
+      }
+
+      this.setState({ featuredItems });
+    }
+  }
+
+  render() {
+    const { featuredItems } = this.state;
+    const { robes, history, featured } = this.props;
+
+    const featuredItemsInSlider = featuredItems.map(item => (
+      <FullRobe robe={item} key={'slider-item-' + item._id} history={history} />
+    ));
+
+    const sliderSettings = {
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      easing: 'ease-in',
+      responsive: [
+        {
+          breakpoint: 680,
+          settings: {
+            arrows: false
+          }
+        }
+      ]
+    };
+
+    if (featured.length) {
+      return (
+        <div id="products">
+          <Slider {...sliderSettings} className="card-slider">
+            {featuredItemsInSlider}
+          </Slider>
+
+          <Robes robes={robes} history={history} showUserControls={true} />
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+}
+
+export default Products;
