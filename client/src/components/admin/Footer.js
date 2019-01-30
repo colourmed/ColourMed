47862a8e9ed12439c43bf3322801a7224098113e
Cristi Cismas <cristicismas99@gmail.com>
2019-01-30 18:49:42 +0200

    Add an option to logout to admin's footer

--- client/src/components/admin/Footer.js
+++ client/src/components/admin/Footer.js
@@ -1,17 +1,23 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import '../../css/universal/Footer.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { logout } from "../../store/actions/auth";
+import "../../css/universal/Footer.css";
 
 class Footer extends Component {
   render() {
     return (
       <div id="footer">
-        <Link to="/">
-          ColourMed
+        <Link to="/">ColourMed</Link>
+        <Link to="/" onClick={() => this.props.logout()}>
+          Log Out
         </Link>
       </div>
     );
   }
 }
 
-export default Footer;
+export default connect(
+  null,
+  { logout }
+)(Footer);
