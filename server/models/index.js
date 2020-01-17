fb011d9da504be04eedffb0afe56dcbd00e41226
Cristi Cismas <cristicismas99@gmail.com>
2020-01-17 21:39:14 +0200

    Add useUnifiedTopology and useCreateIndex to mongoose settings

--- server/models/index.js
+++ server/models/index.js
@@ -4,13 +4,12 @@ const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = Promise;
 
-mongoose.connect(
-  process.env.DB_URL,
-  {
-    keepAlive: true,
-    useNewUrlParser: true
-  }
-);
+mongoose.connect(process.env.DB_URL, {
+  keepAlive: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
 
 module.exports.Admin = require('./admin');
 module.exports.Robe = require('./robe');
